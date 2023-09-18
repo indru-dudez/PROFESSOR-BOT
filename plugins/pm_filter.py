c8b45f441901d14be38212835e4211e57b1b10a7
Soul pc <119080077+soulpc@users.noreply.github.com>
2023-09-18 22:57:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -17,7 +17,7 @@ logger.setLevel(logging.ERROR)
 @Client.on_message(filters.private & filters.text)
 async def reply_to_private_message(client, message):
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔰 JOIN ❗️G R O U P 🔰", url="https://t.me/+oUGIleD44kdlYWM9")]])
-    reply_text = 
+    reply_text = (
         "Just type the movie name in the group. I can only work in groups\n"
         "\nഇവിടെ ചോദിച്ചാൽ സിനിമ കിട്ടില്ല ഗ്രൂപ്പിൽ മാത്രം സിനിമ ചോദിക്കുക\n"
         "\nask in Group Link and👇"
